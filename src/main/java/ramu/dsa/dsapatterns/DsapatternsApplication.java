ee4dfbb0ebba2039013e5c32fddfc46fa7d706ff
stackflowstudio <stackflowstudio@gmail.com>
2026-06-28 14:26:41 +0530

    first commit

---
+++ src/main/java/ramu/dsa/dsapatterns/DsapatternsApplication.java
@@ -0,0 +1,13 @@
+package ramu.dsa.dsapatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsapatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsapatternsApplication.class, args);
+	}
+
+}
